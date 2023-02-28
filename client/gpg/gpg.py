605c5bc7b8a5dc6b1188d63b884193b5396197a7
Platy <daryl.limkz@gmail.com>
2023-02-28 10:08:55 +0800

    feat(client): added ability to add additional options
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- client/gpg/gpg.py
+++ client/gpg/gpg.py
@@ -2,17 +2,20 @@
 import subprocess
 
 
-def sign(key_id: str, data: bytes) -> bytes:
+def sign(key_id: str, data: bytes, options: list = []) -> bytes:
     """
     Signs data and returns output as bytes
 
     :param key_id: Key To Use
     :param data: Data To Sign
+    :param options: Additional Options
     :return: Signed Data
     """
-    process = subprocess.Popen(["gpg", "--local-user", key_id, "--output", "-", "--sign"],
+    process = subprocess.Popen(["gpg", "--local-user", key_id, "--output", "-"] + options +
+                               ["--sign"],
                                stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE)
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
     output, error = process.communicate(input=data)
 
     if error:
@@ -21,15 +24,16 @@ def sign(key_id: str, data: bytes) -> bytes:
     return output
 
 
-def verify(key_id: str, data: bytes) -> bool:
+def verify(key_id: str, data: bytes, options: list = []) -> bool:
     """
     Verify data and returns output boolean
 
     :param key_id: Key To Use
     :param data: Data To Verify
+    :param options: Additional Options
     :return: Boolean Of Successful Verification
     """
-    process = subprocess.Popen(["gpg", "--verify"],
+    process = subprocess.Popen(["gpg"] + options + ["--verify"],
                                stdin=subprocess.PIPE,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
@@ -38,17 +42,20 @@ def verify(key_id: str, data: bytes) -> bool:
     return f"<{key_id}>".encode() in output and process.wait() == 0
 
 
-def encrypt(key_id: str, data: bytes) -> bytes:
+def encrypt(key_id: str, data: bytes, options: list = []) -> bytes:
     """
     Encrypt data and returns output as bytes
 
     :param key_id: Key To Use
     :param data: Data To Encrypt
+    :param options: Additional Options
     :return: Encrypted Data
     """
-    process = subprocess.Popen(["gpg", "--recipient", key_id, "--output", "-", "--encrypt"],
+    process = subprocess.Popen(["gpg", "--recipient", key_id, "--output", "-"] + options +
+                               ["--encrypt"],
                                stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE)
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
     output, error = process.communicate(input=data)
 
     if error:
@@ -57,15 +64,16 @@ def encrypt(key_id: str, data: bytes) -> bytes:
     return output
 
 
-def decrypt(key_id: str, data: bytes) -> bytes:
+def decrypt(key_id: str, data: bytes, options: list = []) -> bytes:
     """
     Decrypt data and returns output as bytes
 
     :param key_id: Key To Use
     :param data: Data To Decrypt
+    :param options: Additional Options
     :return: Decrypted Data
     """
-    process = subprocess.Popen(["gpg", "--decrypt"],
+    process = subprocess.Popen(["gpg"] + options + ["--decrypt"],
                                stdin=subprocess.PIPE,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
@@ -77,14 +85,15 @@ def decrypt(key_id: str, data: bytes) -> bytes:
     return output
 
 
-def import_key(data: bytes) -> bool:
+def import_key(data: bytes, options: list = []) -> bool:
     """
     Imports key and returns output as boolean
 
     :param data: Public Key In Bytes
+    :param options: Additional Options
     :return: Boolean of successful import
     """
-    process = subprocess.Popen(["gpg", "--import"],
+    process = subprocess.Popen(["gpg"] + options + ["--import"],
                                stdin=subprocess.PIPE,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
@@ -93,14 +102,15 @@ def import_key(data: bytes) -> bool:
     return process.wait() == 0
 
 
-def export_key(key_id: str) -> bytes:
+def export_key(key_id: str, options: list = []) -> bytes:
     """
     Returns public key as bytes
 
     :param key_id: Key To Export
+    :param options: Additional Options
     :return: Exported Key
     """
-    process = subprocess.Popen(["gpg", "--export", key_id],
+    process = subprocess.Popen(["gpg"] + options + ["--export", key_id],
                                stdin=subprocess.PIPE,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)

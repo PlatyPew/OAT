a6d0a3b5c2d77204b67f8ee416c7fd45cfd25380
S0meDev99 <90775954+S0meDev99@users.noreply.github.com>
2023-03-02 16:52:42 +0800

    refactor(server): removed unused index.js and require()

--- server/server.js
+++ server/server.js
@@ -2,9 +2,6 @@
 const express = require('express');
 const mongoose = require("mongoose");
 
-// OAK Module
-// const oak = require('./index');
-
 // Setup the express server
 const app = express();
 const PORT = 8080;
@@ -14,14 +11,10 @@ app.use(express.json({limit:"8mb"}));
 
 // User Credential Authentication + Generate IV
 const auth = require("./src/routes/init");
-app.use("/api/auth", auth);
+app.use("/api/init", auth);
 
 const request = require("./src/routes/request");
 app.use("/api/request", request);
-
-// token = oak.generateNewToken("3615f80c9d293ed7402687f94b22d58e529b8cc7916f8fac7fddf7fbd5af4cf777d3d795a7a00a16bf7e7f3fb9561ee9baae480da9fe7a18769e71886b03f315");
-
-// console.log(token) // "Some User token"
 
 const MONGO = "mongodb://localhost:8000/oak"
 

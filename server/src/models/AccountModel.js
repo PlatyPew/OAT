8792635ac151b1d5133700bdb9ca52c4b995bc84
S0meDev99 <2101021@sit.singaporetech.edu.sg>
2023-03-04 04:02:28 +0800

    feat(server): fixed errors for transmission and storage
    -mongodb api keys String -> Buffer
    -added await to rollToken in update.js

--- server/src/models/AccountModel.js
+++ server/src/models/AccountModel.js
@@ -8,8 +8,8 @@ const AccountInfoSchema = new mongoose.Schema(
         email: { type: "String" },
         password: { type: "String" },
         gpgKeyId: { type: "String" },
-        prevApiKey: { type: "String" },
-        nextApiKey: { type: "String" }
+        prevApiKey: { type: "Buffer" },
+        nextApiKey: { type: "Buffer" }
     },
     { collection: "accounts", versionKey: false }
 );

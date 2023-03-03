62d72acddb297b8ad0669e3ffa0ac9ebf183bd5a
S0meDev99 <2101021@sit.singaporetech.edu.sg>
2023-03-04 02:34:43 +0800

    misc(server): account authentication streamlined
    -throw error instead of return boolean for http response 503 in init.js
    -removed unused code

--- server/src/utils/auth.js
+++ server/src/utils/auth.js
@@ -13,55 +13,12 @@ const gpg = require('./gpg');
 const verifyCredentials = async (email, password) => {
     const acc = await AccountInfoModel.findOne({ email: email });
 
-    if (!acc) return false;
+    if (!acc) throw new Error("Email and password incorrect");
 
-    if (acc.password !== password) return false;
+    if (acc.password !== password) throw new Error("Email and password incorrect");
 
     return true;
 };
-
-// const getGpgUid = async (search, type) => {
-//     let acc, gpgUid = false;
-//     switch(type) {
-//         case "email":
-//             // Query MongoDB for Document with email
-//             acc = await AccountInfoModel.findOne({ email: search });
-
-//             if (!acc) return gpgUid;
-
-//             // Store GPG uid
-//             gpgUid = acc.gpgUid;
-
-//             break;
-//         case "token":
-//             // Query MongoDB for Document with token
-//             acc = await AccountInfoModel.findOne({ $or: [
-//                 { prevToken: search },
-//                 { nextToken: search }
-//             ]});
-
-//             if (!acc) return gpgUid;
-
-//             // Store GPG uid
-//             gpgUid = acc.gpgUid;
-
-//             break;
-//         default:
-//             break;
-//     }
-//     return gpgUid;
-// };
-
-// const setGpgUid = async (email, gpgUid) => {
-//     const acc = await AccountInfoModel.findOne({ email: email });
-
-//     if (!acc) return false;
-
-//     acc.gpgUid = gpgUid;
-//     acc.save();
-
-//     return true;
-// };
 
 module.exports = {
     verifyCredentials: verifyCredentials

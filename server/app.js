09d3588cec7f897a237858421068ad50b14e81eb
Platy <daryl.limkz@gmail.com>
2023-03-05 23:03:24 +0800

    build(server): init database
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- server/app.js
+++ server/app.js
@@ -16,35 +16,41 @@ const PORT = 3000;
 // URI for Mongodb
 const MONGO = "mongodb://localhost/oak";
 
-const { AccountInfoModel } = require("./src/models/AccountModel");
-const { InventoryInfoModel } = require("./src/models/MarketModel");
-
 // Initialise database
-AccountInfoModel.insertMany([
-    {
-        email: "admin@oak.com",
-        password:
-            "d9f7cd77d0695cff5e23862024f3cc07e24606dd168ba7593f665a89ced9e972d20c78680c6cb65a4264e496f3e143df3ec9a7efa4f847d7acfee0f39b6e6b2b",
-        admin: true,
-    },
-    {
-        email: "daryl@oak.com",
-        password:
-            "a95a503d26a8d0282f213b7b692057a10f74ac11b9e686755498765900acbf54a88affd2ad28538752084814332e965a88b1d1c8e070f4423b9f150cf6174448",
-        admin: false,
-    },
-]);
+(async () => {
+    const { AccountInfoModel } = require("./src/models/AccountModel");
+    const { InventoryInfoModel } = require("./src/models/MarketModel");
 
-InventoryInfoModel.insertMany([
-    {
-        banana: 25,
-        grape: 23,
-        apple: 17,
-        watermelon: 4,
-        orange: 41,
-        lemon: 29,
-    },
-]);
+    if ((await AccountInfoModel.find()).length === 0) {
+        AccountInfoModel.insertMany([
+            {
+                email: "admin@oak.com",
+                password:
+                    "d9f7cd77d0695cff5e23862024f3cc07e24606dd168ba7593f665a89ced9e972d20c78680c6cb65a4264e496f3e143df3ec9a7efa4f847d7acfee0f39b6e6b2b",
+                admin: true,
+            },
+            {
+                email: "daryl@oak.com",
+                password:
+                    "a95a503d26a8d0282f213b7b692057a10f74ac11b9e686755498765900acbf54a88affd2ad28538752084814332e965a88b1d1c8e070f4423b9f150cf6174448",
+                admin: false,
+            },
+        ]);
+    }
+
+    if ((await InventoryInfoModel.find()).length === 0) {
+        InventoryInfoModel.insertMany([
+            {
+                banana: 25,
+                grape: 23,
+                apple: 17,
+                watermelon: 4,
+                orange: 41,
+                lemon: 29,
+            },
+        ]);
+    }
+})();
 
 // HTTPS settings
 const SSL_OPTIONS = {
